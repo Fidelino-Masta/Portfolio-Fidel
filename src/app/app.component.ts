b365a117c320a651a4cadfe67f066ff068e0722b
Fidelino-Masta <fernandofidelino9@gmail.com>
2024-04-30 10:53:40 +0100

    Adding cards in the technologies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,63 @@ export class AppComponent {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
+
+  cardAngular = false
+  cardCss = false
+  cardHtml = false
+  cardJs = false
+  cardJava = false
+  cardTypescript = false
+  cardSass = false
+
+  mouseEnterAngular() {
+    this.cardAngular = true
+  }
+  mouseOutAngular() {
+    this.cardAngular = false
+  }
+
+
+  mouseEnterCss() {
+    this.cardCss = true
+  }
+  mouseOutCss() {
+    this.cardCss = false
+  }
+
+
+  mouseEnterHtml() {
+    this.cardHtml = true
+  }
+  mouseOutHtml() {
+    this.cardHtml = false
+  }
+
+  mouseEnterJs() {
+    this.cardJs = true
+  }
+  mouseOutJs() {
+    this.cardJs = false
+  }
+
+  mouseEnterJava() {
+    this.cardJava = true
+  }
+  mouseOutJava() {
+    this.cardJava = false
+  }
+
+  mouseEnterTypescript() {
+    this.cardTypescript = true
+  }
+  mouseOutTypescript() {
+    this.cardTypescript = false
+  }
+
+  mouseEnterSass() {
+    this.cardSass = true
+  }
+  mouseOutSass() {
+    this.cardSass = false
+  }
 }
