eb75bb5554219927fff9d124585277aad04e97e4
Fidelino-Masta <fernandofidelino9@gmail.com>
2024-05-01 17:02:29 +0100

    improving the my projects block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { ProjectsWebComponent } from './pages/projects-web/projects-web.component';
 import { ProjectsDesignWebComponent } from './pages/projects-design-web/projects-design-web.component';
 import { ProjectsAppMobileComponent } from './pages/projects-app-mobile/projects-app-mobile.component';
-import { ModalSeeMoreComponent } from './pages/modal-see-more/modal-see-more.component';
-import { ContentModalComponent } from './pages/content-modal/content-modal.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
@@ -16,8 +14,6 @@ import { CommonModule } from '@angular/common';
     ProjectsWebComponent,
     ProjectsDesignWebComponent,
     ProjectsAppMobileComponent,
-    ModalSeeMoreComponent,
-    ContentModalComponent
   ],
   imports: [
     BrowserModule,
